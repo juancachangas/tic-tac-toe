8a0a01cac2810be0f5b4c632ca9423a1e175f6c7
Juan Hincapie <juan.hincapie.is@gmail.com>
2018-09-23 02:34:43 -0500

    Repository initialization - Added blank test files for components

---
+++ src/components/Field.js
@@ -0,0 +1,23 @@
+/*
+ * Replace the `alert` with the actual dispatch later on
+ *
+ * You already have utilit classes available to mark the current field selected by a player:
+ *  .is-player-0
+ *  .is-player-1
+ *
+ * Check out the src/index.html for pre-defined basic css classes.
+ */
+import React from 'react';
+
+const Field = ({
+  id,
+}) => (
+  <div
+    className="Field"
+    role="presentation"
+  >
+    {id}
+  </div>
+);
+
+export default Field;
